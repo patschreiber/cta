f42c1210b1ff1a986997962bdee932132bc5bd7f
patricks <patrick@nori.nu>
2014-04-01 17:01:42 -0500

    javascript work on selecting stops

--- js/cta.js
+++ js/cta.js
@@ -18,18 +18,68 @@ $(document).ready(function() {
 
 
   $('li').click(function() {
-
-    if( $(this).hasClass("selected") ) {
-      $(this).removeClass("selected");
+    var stopName = $(this).text();
+    if( $(this).hasClass("transfer") ) {
+      if( $(this).hasClass("selected") ) {
+        $(this).removeClass("selected");
+        $('li:contains(' + stopName + ')' ).removeClass("selected");
+      }
+      else {
+        $(this).addClass("selected");
+        $('li:contains(' + stopName + ')' ).addClass("selected");
+      }
     }
     else {
-      $(this).addClass("selected");
-    } 
+      if( $(this).hasClass("selected") ) {
+        $(this).removeClass("selected");
+      }
+      else {
+        $(this).addClass("selected");
+      } 
+    }
+  });
+
+  $('.selection').click(function() {
+    var column = $(this).parent().find('ul').find('li');
+    var stopsArray = [];
+
+    if(column.hasClass("selected")) {
+      column.each(function() {
+        if( $(this).hasClass("transfer") ) {
+          stopsArray.push($(this).text());
+        }
+      });
+      $.each(stopsArray, function(index, value) {
+        $("li").filter(function() {
+          return $(this).text() === value;
+        }).removeClass("selected");
+      });
+      column.removeClass("selected");
+    }
+    else {
+
+      column.each(function() {
+        if( $(this).hasClass("transfer") ) {
+          stopsArray.push($(this).text());
+        }
+      });
+      $.each(stopsArray, function(index, value) {
+        $("li").filter(function() {
+          return $(this).text() === value;
+        }).addClass("selected");
+      });
+      column.addClass("selected");
+    }
   });
 
 
 
   $('.percentage-button').click(function() {
+
+
+    generateChart();
+
+
     $('#percentageChart').fadeIn("slow");
 
     //Find how many lines are currently available to ride.
@@ -44,7 +94,7 @@ $(document).ready(function() {
 
     //Find how many lines the user has rode.
     var rode = $('.lines').find(".selected").length;
-    console.log(rode);
+
     if (!displayCtaLineRodeCount) {
       $('.data').append("<p class=\"your-stats\">You've ridden " + rode + " train stops...</p>");
       displayCtaLineRodeCount = true;
@@ -55,7 +105,7 @@ $(document).ready(function() {
 
     //Calculate percentage of train stops a user has visited.
     var percentRode = calculatePercentage(rode, n);
-    console.log(percentRode);
+
     if (!displayCtaPercentRode) {
       $('.data').append("<p class =\"percentRode\">...which amounts to you riding <span class=\"percent-num\">" + percentRode + "%</span> of all the train lines available on the CTA!</p>");
       displayCtaPercentRode = true;
@@ -74,40 +124,29 @@ $(document).ready(function() {
     else {
       percent = 0;
     }
-
     return percent;
   };
 
 
-  $('.selection').click(function() {
-    var column = $(this).parent().find('ul').find('li');
-
-    if(column.hasClass("selected")) {
-      column.removeClass("selected");
+  function generateChart() {
+    context = document.getElementById('percentageChart').getContext('2d');
+    var Radar = {}
+    var data = {
+      labels : ["Red Line","Blue Line","Brown Line","Green Line","Orange Line","Purple Line","Pink Line","Yellow Line"],
+      datasets : [
+        {
+          fillColor : "rgba(151,187,205,0.5)",
+          strokeColor : "rgba(151,187,205,1)",
+          pointColor : "rgba(151,187,205,1)",
+          pointStrokeColor : "#fff",
+          data : [33,10,10,10,5,10,10,10]
+        }
+      ]
     }
-    else {
-      column.addClass("selected");
-    }
-  });
 
-
-  context = document.getElementById('percentageChart').getContext('2d');
-  var Radar = {}
-  var data = {
-    labels : ["Red Line","Blue Line","Brown Line","Green Line","Orange Line","Purple Line","Pink Line","Yellow Line"],
-    datasets : [
-      {
-        fillColor : "rgba(151,187,205,0.5)",
-        strokeColor : "rgba(151,187,205,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        data : [33,10,10,10,5,10,10,10]
-      }
-    ]
+    // LOOK AT THIS CONSOLE LOG TO GET INFO ON HOW TO GET A HANDLE ON THE DATA NUMBERS
+    console.log(data.datasets[0].data[4]);
+    new Chart(context).Radar(data);
   }
-
-// LOOK AT THIS CONSOLE LOG TO GET INFO ON HOW TO GET A HANDLE ON THE DATA NUMBERS
-console.log(data.datasets);
-  new Chart(context).Radar(data);
 
 });
